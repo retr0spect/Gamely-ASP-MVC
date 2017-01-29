b8492c1b01dbf1d6090895ff04214ec42ecb94d3
Aditya Chandel <aditya.chandel@aggiemail.usu.edu>
2017-01-29 16:00:11 -0700

    Added MembershipType model

--- Gamely/Controllers/CustomersController.cs
+++ Gamely/Controllers/CustomersController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
 using Gamely.Models;
@@ -9,25 +10,29 @@ namespace Gamely.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = getCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
 
-        private IEnumerable<Customer> getCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer {Id = 1, Name = "Aditya"},
-                new Customer {Id = 2, Name = "Ronald"}
-            };
-        }
-
+        // GET: Customers/Details
         public ActionResult Details(int id)
         {
-            var customer = getCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
